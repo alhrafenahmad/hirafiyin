bbc3cef1ea167f93347325bd7d6af93a730a4bf0
alhrafenahmad <alhrafenahmad@gmail.com>
2025-12-24 22:22:55 +0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -31,7 +31,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "f0760fc2bad874c4e22e6fe154306193
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "987bc46b1c0eabf8b66267f58e4528d3",
+"flutter_bootstrap.js": "58190d334b544307dc3bab414ba377db",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
